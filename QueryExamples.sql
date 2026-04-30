23420de213cf525f53d365fc704fff5b75d3cecb
KnotFaries <205455908+KnotFaries@users.noreply.github.com>
2026-04-30 19:49:46 -0400

    moved code
    moved example from quick view to query example

--- QueryExamples.sql
+++ QueryExamples.sql
@@ -1,3 +1,2 @@
-
-
+-- Use to search for key terms in titles
 SELECT * FROM title_authorname_table where title LIKE '%Bless%';
--- QuickViews.sql
+++ QuickViews.sql
@@ -1,6 +1,3 @@
 SELECT * FROM story Order BY(story_id) DESC;
 SELECT * FROM author Order BY(author_id) DESC;
-SELECT * FROM title_authorname_table Order BY(story_id) DESC;
-
-Call newstory (238,'TestTitle', 'TestSource', 83, 'TestUname', 'TestFname', 'TestLname')
-CALL delete_story(238, 83)
+SELECT * FROM title_authorname_table ORDER BY (title);
